fffc976550860ba6c69b988601d4ea3be680f336
junho park <junho7778@travelai.io>
2021-10-26 19:03:28 +0900

    2021/10/26

--- src/main/java/com/example/demo2/Demo2Application.java
+++ src/main/java/com/example/demo2/Demo2Application.java
@@ -2,12 +2,26 @@ package com.example.demo2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@RequestMapping("/front")
+@Controller
 @SpringBootApplication
 public class Demo2Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Demo2Application.class, args);
     }
+
+    @GetMapping("/{htmlPath}")
+    public String redirect(@PathVariable String htmlPath){
+        return "/front/".concat(htmlPath);
+    }
+
+
+
 
 }
---
+++ src/main/resources/templates/js/navigator.js
@@ -0,0 +1,16 @@
+
+$(document).ready(function() {
+
+
+    $(".gnb>li>a").click(function (){
+        $(this).next().slideToggle();
+        $(this).html(function (i,oldText){
+            if(oldText[oldText.length-1]==="▼") {
+                return oldText.substring(0, oldText.length - 1) + "▶"
+            }else {
+                return oldText.substring(0, oldText.length - 1) + "▼"
+
+            }
+        });
+    })
+});
