36e59bbe4d0b1433ff23233dd82ff48c17094b4a
junho park <junho7778@travelai.io>
2021-10-28 18:47:06 +0900

    2021/10/28

---
+++ src/main/resources/templates/admin_ui/js/header.js
@@ -0,0 +1,8 @@
+
+
+
+$(document).ready(function() {
+    $( "header" ).load( "headers.html"  ,function (){
+
+    });
+});
---
+++ src/main/resources/templates/admin_ui/js/navigator.js
@@ -0,0 +1,21 @@
+
+
+
+$(document).ready(function() {
+    $( "nav" ).load( "navigator.html .gnb"  ,function (){
+        $(".gnb>li>a").click(function (){
+            $(this).next().slideToggle();
+            $(this).html(function (i,oldText){
+                if(oldText[oldText.length-1]==="▼") {
+                    return oldText.substring(0, oldText.length - 1) + "▶"
+                }else {
+                    return oldText.substring(0, oldText.length - 1) + "▼"
+
+                }
+            });
+        })
+    });
+
+
+
+});
