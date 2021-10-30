00b636f602bcfd164e20c7a8e95e3f6b8f05eb18
junho park <junho7778@travelai.io>
2021-10-30 18:29:19 +0900

    2021/10/30

--- src/main/resources/templates/admin_ui/user_info/user_info.js
+++ src/main/resources/templates/admin_ui/user_info/user_info.js
@@ -3,12 +3,47 @@ $(document).ready(function() {
     // $("#character_info").load("character_info.html")
     $(".tab_title li").click(function() {
         var idx = $(this).index();
-
+        $("#member_info").load("member_info.html")
+        $("#character_info").load("character_info.html")
+        $("#skill").load("skill.html")
+        $("#equipment").load("equipment.html")
+        $("#stat").load("stat.html")
+        $("#constellation").load("constellation.html")
+        $("#seal").load("seal.html")
+        $("#housing").load("housing.html")
+        $("#mission").load("mission.html")
+        $("#quest").load("quest.html")
+        $("#mailbox").load("mailbox.html")
 
         $(".tab_title li").removeClass("on");
         $(".tab_title li").eq(idx).addClass("on");
         $(".tab_cont > div").hide();
         $(".tab_cont > div").eq(idx).show();
 
+        if (idx==0){
+            $("#member_info").load("member_info.html")
+        }else if(idx==1){
+            $("#character_info").load("character_info.html")
+        }else if(idx==2){
+            $("#skill").load("skill.html")
+        }else if(idx==3){
+            $("#equipment").load("equipment.html")
+        }else if(idx==4){
+            $("#stat").load("stat.html")
+        } else if(idx==5){
+            $("#constellation").load("constellation.html")
+        }else if(idx==6){
+            $("#equipment").load("equipment.html")
+        }else if(idx==7){
+            $("#seal").load("seal.html")
+        }else if(idx==8){
+            $("#housing").load("housing.html")
+        }else if(idx==9){
+            $("#mission").load("mission.html")
+        }else if(idx==10){
+            $("#quest").load("quest.html")
+        }else if(idx==11){
+            $("#mailbox").load("mailbox.html")
+        }
     })
 });
